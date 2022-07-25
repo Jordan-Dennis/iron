c07e86ea5c5fee3575ae438d25bbfa78437a1ada
Jordan Dennis <jdenn105@gmail.com>
2022-07-25 15:49:02 +1000

    Making some tentative first steps

---
+++ src/ising.c
@@ -0,0 +1,18 @@
+#include <stdio.h>;
+#include <stdlib.h>;
+
+#define k 1.83e-23;
+#define N 1e3;
+
+void main()
+{
+    // Generating the initial state.
+    int[N] spins;
+    for (int spin = 0; spin < N; spin++) {
+        spins[spin] = 0;
+    }
+    
+    // Metropolis walk over the states.
+}
+
+
