6b2f3654ead43bc7988ca13fb2e9707be49f11a0
Jordan Dennis <jdenn105@gmail.com>
2022-07-29 12:18:46 +1000

    Expanding the capabilities of my bash sript.

--- build.sh
+++ build.sh
@@ -1,12 +1,34 @@
-# 
-#
+# build.sh 
+# --------
+# Contains useful abreviations for working on the project. 
+# An attempt at a more wholesome and involved experience 
+# that simplifies commonly used functionality.
 
+
+# build
+# -----
+# Compile the c code of the simulation in src/ising.c to 
+# machine code in the out directory as ising.
 build() {
     gcc -o out/ising src/ising.c -lm
 }
 
 
+# run
+# ---
+# Excecute the executable created by build. These two will 
+# nearly always be run together. 
+# 
+# parameters
+# ----------
+# 
 run() {
+    while getopts ":hl:" option; do
+        case $option in
+            h) echo TODO:;;
+            l) build;;
+        esac
+    done
     out/ising
 }
 
