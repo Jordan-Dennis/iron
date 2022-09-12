ca7833d84e5970fd448d34c7dc90597138783a37
Jordan Dennis <jdenn105@gmail.com>
2022-09-12 11:25:34 +1000

    Working out the error for the heat capacity and possibly solving the entropy. Starting to type out the assignment.

--- saint.py
+++ saint.py
@@ -7,58 +7,60 @@ from scipy.sparse import spdiags,linalg,eye
 
 
 
-def initialstate(N):   
+def initial_state(number: int) -> int:   
     ''' 
     Generates a random spin configuration for initial condition
     '''
-    state = 2*np.random.randint(2, size=(N,N))-1
-    return state
+    return 2 * np.random.randint(2, size=(number, number)) - 1
 
 
-
-def mcmove(config, beta):
+def mcmove(spins, temperature):
     '''
     Monte Carlo move using Metropolis algorithm 
     '''
+    rows, columns = spins.shape
+
+    for _ in np.arange(spins.size):
+        row = np.random.randint(0, rows)
+        column = np.random.randint(0, columns)
+
+        neighbours = spins[(row + 1) % rows, column] 
+        neighbours += spins[row, (column + 1) % columns] 
+        neighbours += spins[(row - 1) % rows, column] 
+        neighbours += spins[row, (column - 1) % columns]
+
+        cost = 2 * spins[row, column] * neighbours
+        
+        if (cost < 0) or (rand() < np.exp(-cost / temperature)):
+            spins[row, column] *= -1
     
-    for i in range(N):
-        for j in range(N):
-                a = np.random.randint(0, N)
-                b = np.random.randint(0, N)
-                s =  config[a, b]
-                nb = config[(a+1)%N,b] + config[a,(b+1)%N] + config[(a-1)%N,b] + config[a,(b-1)%N]
-                cost = 2*s*nb
-                
-                if cost < 0:
-                    s *= -1
-                elif rand() < np.exp(-cost*beta):
-                    s *= -1
-                config[a, b] = s
-    return config
+    return spins
 
 
 
-def calcEnergy(config):
+def calc_energy(spins: int) -> float:
     '''
     Energy of a given configuration
     '''
     energy = 0 
+   
+    rows, columns = spins.shape
+    for row in np.arange(rows):
+        for column in np.arange(columns):
+            neighbours = spins[(row + 1) % rows, column] 
+            neighbours += spins[row, (column + 1) % columns] 
+            neighbours += spins[(row - 1) % rows, column] 
+            neighbours += spins[row, (column - 1) % columns]
+            energy += -neighbours * spins[row, column]
     
-    for i in range(len(config)):
-        for j in range(len(config)):
-            S = config[i,j]
-            nb = config[(i+1)%N, j] + config[i,(j+1)%N] + config[(i-1)%N, j] + config[i,(j-1)%N]
-            energy += -nb*S
-    return energy/2.  # to compensate for over-counting
+    return energy / 2.  # to compensate for over-counting
 
 
-
-def calcMag(config):
+def calc_magnetisation(spins: int) -> float:
     '''
     Magnetization of a given configuration
     '''
-    mag = np.sum(config)
-    return mag
+    return np.sum(spins)
 
 
 def calc_entropy(spins: int) -> float:
@@ -73,90 +75,84 @@ def calc_entropy(spins: int) -> float:
 
     aligned = aligned / 4
 
-    entropy = spins.size * np.log(spins.size)
+    entropy = rows * np.log(rows)
     entropy -= aligned * np.log(aligned) 
     entropy -= (spins.size - aligned) * np.log(spins.size - aligned)
     
     return entropy
         
-## NOTE: change these parameters for a smaller and faster simulation 
-#----------------------------------------------------------------------
+number = 10         #  size of the lattice, N x N
+warm_up = 10**2       #  number of MC sweeps for equilibration
+steps = 10**4  #  number of MC sweeps for calculation
 
 
-nt      = 32         #  number of temperature points
-N       = 10         #  size of the lattice, N x N
-eqSteps = 10**2       #  number of MC sweeps for equilibration
-mcSteps = 10**4  #  number of MC sweeps for calculation
+temperatures = np.linspace(1.53, 3.28, 32); 
+energies = np.zeros((temperatures.size, 2), dtype=float)
+entropies = np.zeros((temperatures.size, 2), dtype=float)
+free_energies = np.zeros((temperatures.size, 2), dtype=float)
+magnetisations = np.zeros((temperatures.size, 2), dtype=float)
+heat_capacities = np.zeros((temperatures.size, 2), dtype=float)
 
+for temp, temperature in enumerate(temperatures):
+    spins = initial_state(number)         # initialise
 
-T       = np.linspace(1.53, 3.28, nt); 
-E,M,C,S = np.zeros(nt), np.zeros(nt), np.zeros(nt), np.zeros(nt)
-n1, n2  = 1.0/(mcSteps*N*N), 1.0/(mcSteps*mcSteps*N*N) 
-# divide by number of samples, and by system size to get intensive values
-#  MAIN PART OF THE CODE
-#----------------------------------------------------------------------
+    energy = np.zeros((steps,), dtype=float)
+    entropy = np.zeros((steps,), dtype=float)
+    magnetisation = np.zeros((steps,), dtype=float)
 
+    for _ in range(warm_up):         # equilibrate
+        mcmove(spins, temperature)           # Monte Carlo moves
 
-for tt in range(nt):
-    config = initialstate(N)         # initialise
+    for i in range(steps):
+        mcmove(spins, temperature)           
 
-    _energy = np.zeros((mcSteps,), dtype=float)
-    _entropy = np.zeros((mcSteps,), dtype=float)
-    _magnetisation = np.zeros((mcSteps,), dtype=float)
+        energy[i] = calc_energy(spins)
+        entropy[i] = calc_entropy(spins)
+        magnetisation[i] = calc_magnetisation(spins)
 
-    iT = 1.0 / T[tt]
-    iT2 = iT * iT
-    
-    for i in range(eqSteps):         # equilibrate
-        mcmove(config, iT)           # Monte Carlo moves
-
-    for i in range(mcSteps):
-        mcmove(config, iT)           
-        Ene = calcEnergy(config)     # calculate the energy
-        Mag = calcMag(config)        # calculate the magnetisation
-
-        _energy[i] = calc_energy(spins)
-        
-        E1 = E1 + Ene
-        M1 = M1 + Mag
-        M2 = M2 + Mag*Mag 
-        E2 = E2 + Ene*Ene
-        _entropy[i] = calc_entropy(config)
-
-
-    # divide by number of sites and iteractions to obtain intensive values    
-    S[tt] =  entropy
-    E[tt] = n1*E1
-    M[tt] = n1*M1
-    C[tt] = (n1*E2 - n2*E1*E1)*iT2
+    # divide by number of sites and iteractions to obtain intensive values 
+    _energy = np.mean(energy)
+    _energy_var = np.std(energy)
+    _entropy = np.mean(entropy)
+    _entropy_var = np.std(entropy)
+   
+    energies[temp, 0] = _energy / number
+    energies[temp, 1] = _energy_var / number
+    entropies[temp, 0] = _entropy / number
+    entropies[temp, 1] = _entropy_var / number
+    free_energies[temp, 0] = (_energy - temperature * _entropy) / number
+    free_energies[temp, 1] = (_energy_var - temperature * _entropy_var) / number
+    magnetisations[temp, 0] = np.abs(np.mean(magnetisation)) / number
+    magnetisations[temp, 1] = np.std(np.abs(magnetisation)) / number
+    heat_capacities[temp, 0] = (_energy_var ** 2) / number / temperature ** 2
+    heat_capacities[temp, 1] = (np.std(energy ** 2) - 2 * _energy_var) / number / temperature ** 2
 #  plot the calculated values   
 #----------------------------------------------------------------------
-
-print(S)
 
 f = plt.figure(figsize=(18, 10))
 
 sp =  f.add_subplot(2, 2, 1)
-plt.scatter(T, E, s=50, marker='o', color='IndianRed')
+plt.errorbar(temperatures, energies[:, 0], energies[:, 1], s=50, marker='o')
 plt.xlabel("Temperature (T)", fontsize=20)
 plt.ylabel("Energy", fontsize=20)         
 plt.axis('tight')
 
 sp =  f.add_subplot(2, 2, 2)
-plt.scatter(T, abs(M), s=50, marker='o', color='RoyalBlue')
+plt.scatter(temperatures, magnetisations[:, 0], magnetisations[:, 1], s=50, marker='o')
 plt.xlabel("Temperature (T)", fontsize=20) 
 plt.ylabel("Magnetization", fontsize=20)   
 plt.axis('tight')
 
 sp =  f.add_subplot(2, 2, 3)
-plt.scatter(T, C, s=50, marker='o', color='IndianRed')
+plt.scatter(temperatures, heat_capacities[:, 0], heat_capacities[:, 1], s=50, marker='o')
 plt.xlabel("Temperature (T)", fontsize=20)  
 plt.ylabel("Specific Heat", fontsize=20)   
 plt.axis('tight') 
 
 sp =  f.add_subplot(2, 2, 4)
-plt.scatter(T, S, s=50, marker='o', color='IndianRed')
+plt.scatter(temperatures, entropies[:, 0], entropies[:, 1], s=50, marker='o')
 plt.xlabel("Temperature (T)", fontsize=20)
 plt.ylabel("Entropy", fontsize=20)
 plt.axis('tight')
+plt.savefig("pub/figures/q2c.pdf")
 plt.show()
