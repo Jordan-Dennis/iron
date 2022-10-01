bbea036170db8a8676397dcaf0c7c9a77d207d7a
Jordan Dennis <jdenn105@gmail.com>
2022-10-02 09:47:07 +1000

    A beautiful python implementation of the first graphic. This gives me yet hope for the project.

--- src/plots/question_1.py
+++ src/plots/question_1.py
@@ -1,13 +1,22 @@
 import numpy as np
-import maplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-with open("pub/data/1a.csv") as question_1a_data:
-    question_1a_data = np.array([
-        [int(spin) for spin in line.split(",")] for line in question_1a_data])
+
+with open("pub/data/1d_test.csv") as question_1a_data:
+    data = []
+    for line in question_1a_data:
+        if not line.strip().startswith("#"):
+            data.append([int(spin) for spin in line.strip().split(",")])
+
+    data = np.array([data])[0]
+
+print(data.shape)
 
 plt.figure(figsize=(10, 10))
-for temperture in range(3):
+for temperature in range(3):
     plt.subplot(3, 1, temperature + 1)
     plt.title(temperature)
-    plt.imshow(question_1a_data[:2, :])
+    plt.imshow(data[temperature:temperature + 2, :])
+
+plt.show()
 
