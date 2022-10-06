9e572426392bb6aa6a38088cbe520b218e5f54dd
Jordan Dennis <jdenn105@gmail.com>
2022-10-07 08:29:49 +1000

    Doing so parallel computing in c. That's pretty fucking dope

--- src/tests/fork_test.c
+++ src/tests/fork_test.c
@@ -1,23 +1,24 @@
+#include<omp.h> 
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h> 
+#include<unistd.h>
 
 int main(void)
 {
-    // This is a test to see if I can't get fork to work. 
-    int pid = fork();
-    if (pid == 0)
-    {
-        printf("I am in the parent process!");
-    }
-    else
-    {
-        printf("I am in the child process!");
-    }
-    
-    #pragma omp parallel for
+//    int pid = fork();
+//    if (pid == 0)
+//    {
+//        printf("I am in the parent process!\n");
+//    }
+//    else
+//    {
+//        printf("I am in the child process!\n");
+//    }
+ 
+    #pragma omp parallel for num_threads(10)
     for (int i = 0; i < 10; i++)
     {
-        sleep(1000);
+        printf("I am in process %i\n", i);
+        sleep(1);
     }
 }
