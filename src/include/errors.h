70162b358bab7e90a29220518ce51226ea3eba1b
Jordan Dennis <jdenn105@gmail.com>
2022-10-12 08:18:05 +1000

    Removing old and unwanted headers

--- src/include/errors.h
+++
@@ -1,9 +0,0 @@
-#ifndef ERRORS_H
-#define ERRORS_H
-
-void validate_args(int num_args);
-void validate_file(FILE* file, char* file_name);
-void validate_option(char* option);
-void validate_char(char target, char recieved);
-
-#endif
